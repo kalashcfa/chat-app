b3e7ef459cf92e18b184cbd4b40e85b4f2c744ec
kalashcfa <kalashchourasiya.cfa@gmail.com>
2024-08-26 13:24:00 +0530

    added socket.io for handshake

--- chat-app-backend/app.js
+++ chat-app-backend/app.js
@@ -1,20 +1,41 @@
 const exp =  require('express');
 const cors = require('cors');
 const app = exp();
-const http = require('http')
+const http = require('http');
+const socketIo = require('socket.io');
+const server =  http.createServer(app);
+const io = socketIo(server);
 
-const {Socket} = require('socket.io')
+let users = {};
+let group = {};
 
+//-----------------------------handshake--------------------------------- 
+
+io.on('connection',(socket)=>
+{
+    console.log('user is connected...')
+
+
+    socket.on('join',(data) =>
+    {
+        users[socket.id] = {userName : data.userName , status:'active'}
+        io.emit('user-list',Object.values(users))
+    })
+})
+
+
+app.use(cors());
 
 require('dotenv').config()
 
 const port = process.env.PORT || 3000;
 
-app.use(cors());
 
 
-
+//-------------------------------------server start----------------------------------
 app.listen(port,()=>
 {
-    console.log(`server started at ${port} `)
+    console.log(`server started at ${port}`)
 })
+
+
